51cb1d22c1529966ef09dda2b97109606ce25be4
johannes karoff <johannes@karoff.net>
2018-05-08 10:21:41 +0200

    sh functions

--- shfunctions/fzf_functions.sh
+++ shfunctions/fzf_functions.sh
@@ -58,7 +58,7 @@ fco() {
 # example usage: git rebase -i `fcs`
 fsha() {
   local commits commit
-  commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
+  commits=$(git log --all --color=always --pretty=oneline --abbrev-commit --reverse) &&
   commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
   echo -n $(echo "$commit" | sed "s/ .*//")
 }
